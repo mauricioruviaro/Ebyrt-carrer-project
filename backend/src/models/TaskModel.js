f0aa5a3f28726c9646a581876f8f895de273fe0d
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-11-04 19:07:36 -0300

    adicionada funcao para inserir uma task no banco de dados

--- backend/src/models/TaskModel.js
+++ backend/src/models/TaskModel.js
@@ -1,1 +1,11 @@
 const connection = require('./connection');
+
+const addTask = async (task, status) => {
+  const db = await connection();
+  const task = await db.collection('task').insertOne({ task, status });
+  return true;
+};
+
+module.exports = {
+  addTask,
+};
