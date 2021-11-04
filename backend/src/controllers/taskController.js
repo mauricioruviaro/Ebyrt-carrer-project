4187f93012bda228099edfa73852f8f74d6b5e1e
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-11-04 20:19:28 -0300

    update na task controller

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,13 +1,18 @@
+const taskService = require('../services/taskService');
 const {
   addTask,
 } = require('../models/TaskModel');
 
 const somethingIsWrong = 'Something is wrong'
 const taskInserted = 'Task inserted';
+const invalidValues = 'Invalid values';
 
 const addTaskController = async (req, res) => {
   try {
     const { task, status } = req.body;
+    const verifyTasks = taskService.verifyTask(task, status);
+
+    if (!verifyTasks) return res.status(401).json({ message: invalidValues });
 
     await addTask(task, status);
     res.status(201).json({ message: taskInserted });
